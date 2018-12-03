ac523c373798a52affa31b4fb60f9194d4da521d
Joe Martella <joem@microsoft.com>
2018-12-02 22:37:38 -0800

    Updates

---
+++ precache-manifest.79a38c742e1ab0bd2516edab42081d28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a15e582e35af0424fc13",
+    "url": "/big-keyboard/static/js/runtime~main.a15e582e.js"
+  },
+  {
+    "revision": "51f2f28f9bfff73019cc",
+    "url": "/big-keyboard/static/js/main.51f2f28f.chunk.js"
+  },
+  {
+    "revision": "20bc491bae5e667480bb",
+    "url": "/big-keyboard/static/js/1.20bc491b.chunk.js"
+  },
+  {
+    "revision": "51f2f28f9bfff73019cc",
+    "url": "/big-keyboard/static/css/main.3cdfd609.chunk.css"
+  },
+  {
+    "revision": "b0f88f4a69e6097e47af69824ed5984e",
+    "url": "/big-keyboard/index.html"
+  }
+];
